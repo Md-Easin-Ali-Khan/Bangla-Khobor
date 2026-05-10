fa7547f58c09388205f9bab7d76a1a685f0d861c
Md-Easin-Ali-Khane <mdeasinalikhan9@gmail.com>
2026-05-10 11:49:28 +0600

    add breaking news marquee section

--- src/components/shared/BreakingNews.jsx
+++ src/components/shared/BreakingNews.jsx
@@ -1,11 +1,35 @@
 import React from 'react';
 import Marquee from 'react-fast-marquee';
 
+const newsData = [
+    {
+        _id: "1",
+        title: "Breaking News: Government Announces New Digital Media Policy",
+    },
+    {
+        _id: "2",
+        title: "Breaking News: Global Fuel Prices Drop Amid Market Changes",
+    },
+    {
+        _id: "3",
+        title: "Breaking News: Tech Industry Opens New Opportunities for Young Developers",
+    }
+];
+
 const BreakingNews = () => {
     return (
         <div className='flex items-center gap-5 bg-gray-200 py-4'>
             <button className='font-medius text-xl text-white bg-[#D72050] btn'>Latest</button>
-            <Marquee>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit iusto, exercitationem doloribus esse perferendis suscipit autem similique? Repellat quis labore autem ipsum sed nobis, placeat commodi aperiam atque vero optio delectus non modi pariatur aspernatur cumque facilis earum praesentium, dolores saepe facere. Aperiam repellat qui et eligendi reprehenderit, quo maxime accusantium hic placeat, adipisci, asperiores ipsa porro at error officia rem fugit numquam unde assumenda quia? A, exercitationem ipsam quis cupiditate recusandae fugit. Quo voluptatum autem in at asperiores, ipsum nulla non exercitationem distinctio debitis, quas ratione sunt minima modi expedita. Quod maxime veniam deserunt repudiandae, eius hic laborum magni.</Marquee>
+            <Marquee pauseOnHover={true} speed={60}>
+                {newsData.map((breaking) => (
+                    <span
+                        key={breaking._id}
+                        className="mr-16 text-gray-800 font-medium cursor-pointer"
+                    >
+                        {breaking.title}
+                    </span>
+                ))}
+            </Marquee>
         </div>
     );
 };
--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -4,12 +4,11 @@ import Image from 'next/image';
 
 const Header = () => {
     return (
-        <div>
-            <div className='text-center'>
-                <Image src={logo} width={300} height={200} alt='logo' className='mx-auto mb-5'></Image>
-                <p className='font-normal text-lg text-gray-600 mb-2.5'>Journalism Without Fear or Favour</p>
-                <p>{format(new Date(), "EEEE, MMM dd, yyyy")}</p>
-            </div>
+        <div className='text-center mb-7'>
+            <Image src={logo} width={300} height={200} alt='logo' className='mx-auto mb-5'></Image>
+
+            <p className='font-normal text-lg text-gray-600 mb-2.5'>Journalism Without Fear or Favour</p>
+            <p className='font-medium text-xl text-gray-600'>{format(new Date(), "EEEE, MMM dd, yyyy")}</p>
         </div>
     );
 };
